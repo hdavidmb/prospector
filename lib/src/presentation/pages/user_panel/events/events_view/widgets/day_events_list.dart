138bc34df18b4a041de75e7609f176cb3c47fc1b
Herman <hdavidmb@gmail.com>
2021-10-26 16:52:59 -0500

    Extract EventListTile as independent widget

--- lib/src/presentation/pages/user_panel/events/events_view/widgets/day_events_list.dart
+++ lib/src/presentation/pages/user_panel/events/events_view/widgets/day_events_list.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:prospector/src/presentation/theme/theme_providers.dart';
 
 import '../../../../../../features/events/application/events_providers.dart';
 import '../../../../../../features/events/domain/entity/event_entity.dart';
-import '../../../../../core/widgets/dismissible_background.dart';
 import '../../../../../helpers/date_formatters.dart';
 import '../logic/events_view_providers.dart';
+import 'event_list_tile.dart';
 
 class DayEventsList extends ConsumerWidget {
   const DayEventsList({
@@ -31,41 +30,6 @@ class DayEventsList extends ConsumerWidget {
       itemBuilder: (context, index) =>
           EventListTile(event: selectedDayEvents[index]),
       separatorBuilder: (_, __) => const Divider(height: 0.0, indent: 8.0),
-    );
-  }
-}
-
-class EventListTile extends StatelessWidget {
-  const EventListTile({
-    Key? key,
-    required this.event,
-  }) : super(key: key);
-
-  final Event event;
-
-  @override
-  Widget build(BuildContext context) {
-    return Dismissible(
-      key: Key(event.id),
-      direction: DismissDirection.endToStart,
-      background: const DismissibleBackground(),
-      onDismissed: (direction) {
-        context.read(eventsNotifierProvider).deleteEvent(eventID: event.id);
-      },
-      child: ListTile(
-        title: Text(event.title),
-        subtitle: Text(
-          eventTileFormatedDate(
-              selectedDate: context.read(eventsViewProvider).selectedDay,
-              event: event,
-              is24hours: context.read(themeNotifierProvider).is24hours),
-        ),
-        leading:
-            Icon(event.type == 'event' ? Icons.event : Icons.notifications),
-        onTap: () {
-          // TODO navigate to event details or edit page
-        },
-      ),
     );
   }
 }
---
+++ lib/src/presentation/pages/user_panel/events/events_view/widgets/event_list_tile.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../../../features/events/application/events_providers.dart';
+import '../../../../../../features/events/domain/entity/event_entity.dart';
+import '../../../../../core/widgets/dismissible_background.dart';
+import '../../../../../helpers/date_formatters.dart';
+import '../../../../../theme/theme_providers.dart';
+import '../logic/events_view_providers.dart';
+
+class EventListTile extends StatelessWidget {
+  const EventListTile({
+    Key? key,
+    required this.event,
+  }) : super(key: key);
+
+  final Event event;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: Key(event.id),
+      direction: DismissDirection.endToStart,
+      background: const DismissibleBackground(),
+      onDismissed: (direction) {
+        context.read(eventsNotifierProvider).deleteEvent(eventID: event.id);
+      },
+      child: ListTile(
+        title: Text(event.title),
+        subtitle: Text(
+          eventTileFormatedDate(
+              selectedDate: context.read(eventsViewProvider).selectedDay,
+              event: event,
+              is24hours: context.read(themeNotifierProvider).is24hours),
+        ),
+        leading:
+            Icon(event.type == 'event' ? Icons.event : Icons.notifications),
+        onTap: () {
+          // TODO navigate to event details or edit page
+        },
+      ),
+    );
+  }
+}
