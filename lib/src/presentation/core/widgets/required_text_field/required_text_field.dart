3410d0cb286710d9c475323352b997df27bc3c55
Herman <hdavidmb@gmail.com>
2021-11-08 20:47:24 -0500

    Create required textfield to unify contact name and event title textFields

---
+++ lib/src/presentation/core/widgets/required_text_field/required_text_field.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'package:prospector/src/presentation/helpers/form_validators.dart';
+
+class RequiredTextField extends StatelessWidget with FormValidators {
+  final String? initialValue;
+  final String hintText;
+  final String errorMessage;
+  final void Function(String) onChanged;
+
+  const RequiredTextField({
+    Key? key,
+    this.initialValue,
+    required this.hintText,
+    required this.errorMessage,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      initialValue: initialValue,
+      keyboardType: TextInputType.text,
+      textCapitalization: TextCapitalization.words,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.title),
+        hintText: hintText,
+      ),
+      textInputAction: TextInputAction.next,
+      onChanged: onChanged,
+      validator: (value) {
+        final bool isValid = value != null && validateFieldIsNotEmpty(value);
+        return isValid ? null : errorMessage;
+      },
+    );
+  }
+}
