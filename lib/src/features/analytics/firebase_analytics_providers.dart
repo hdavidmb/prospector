d0776df51342078230ef6bcf7189566d3d56b0d6
Herman <hdaivdmb@gmail.com>
2021-11-28 15:51:36 -0500

    Create firebase analytics providers

---
+++ lib/src/features/analytics/firebase_analytics_providers.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:prospector/src/features/analytics/firebase_analytics_service.dart';
+
+final firebaseAnalyticsInstance = Provider<FirebaseAnalytics>((ref) {
+  return FirebaseAnalytics();
+});
+
+final firebaseAnalyticsObserver = Provider<FirebaseAnalyticsObserver>((ref) {
+  return FirebaseAnalyticsObserver(
+      analytics: ref.watch(firebaseAnalyticsInstance));
+});
+
+final firebaseAnalyticsServiceProvider =
+    Provider<FirebaseAnalyticsService>((ref) {
+  return FirebaseAnalyticsService(
+      analytics: ref.watch(firebaseAnalyticsInstance));
+});
---
+++ lib/src/features/analytics/firebase_analytics_service.dart
@@ -0,0 +1,8 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+
+class FirebaseAnalyticsService {
+  final FirebaseAnalytics analytics;
+  FirebaseAnalyticsService({
+    required this.analytics,
+  });
+}
